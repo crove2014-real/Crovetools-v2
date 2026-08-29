d8f9549fc954e8029c4cf3d0e4d1e3d3348a7115
crove2014-real <crove2014@gmail.com>
2026-08-29 11:11:03 +0800

    Delete Crove-Tools-v3.0-60plus directory

--- Crove-Tools-v3.0-60plus/functions/api/ip.js
+++
@@ -1,1 +0,0 @@
-export async function onRequest({request}){const cf=request.cf||{};return new Response(JSON.stringify({ip:request.headers.get("CF-Connecting-IP")||"unknown",country:cf.country||null,city:cf.city||null,region:cf.region||null,colo:cf.colo||null,asn:cf.asn||null},null,2),{headers:{"content-type":"application/json;charset=UTF-8"}})}
